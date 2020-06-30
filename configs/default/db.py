81f4e66efefeed197ba688f06e0acfa0179def91
Wei Zhang <zhangweihec@icloud.com>
2020-06-30 07:43:20 +0200

    fix

--- configs/default/db.py
+++ configs/default/db.py
@@ -1,17 +1,13 @@
 import os
 
 # mysql 设置
-DATABASES = {
-    "default": {
-        "ENGINE": "django_prometheus.db.backends.mysql",
-        "NAME": "conx",
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", "Zw19820130"),
-        "HOST": os.getenv("MYSQL_HOST", "127.0.0.1"),
-        "USER": os.getenv("MYSQL_USER", "root"),
-        "OPTIONS": {
-            "autocommit": True,
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "charset": "utf8mb4",
-        },
+DATABASES_SET_KEY = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'connect-test',
+        'USER': 'zhangwei',
+        'PASSWORD': '593170abcdefg',
+        'HOST': '34.89.17.203',
+        'PORT': '5432',
     }
 }
